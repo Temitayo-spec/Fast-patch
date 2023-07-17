00e07c776f36cfd030bf256f2240916277467035
olawanle temitayo <olawanletemitayo@gmail.com>
2023-07-17 19:17:33 +0100

    setting up sanity for backend

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 import { Stack } from 'expo-router';
 import { Categories, FeaturedRow, HeaderLeft, Search } from '../components';
 import { UserIcon } from 'react-native-heroicons/outline';
